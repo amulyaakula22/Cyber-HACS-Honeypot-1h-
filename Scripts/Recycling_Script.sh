bcd1d5bfa05f0e58e31f294ccf85a3370ac96555
methashaB <Methasha.dd@gmail.com>
2024-09-28 17:17:16 -0400

    actual updated recycling script

--- Scripts/Recycling_Script.sh
+++ Scripts/Recycling_Script.sh
@@ -14,10 +14,13 @@ startTime=$(date -Iseconds)
 
 containerName=$1
 externalIP=$2
+
+
 containerIP=$(sudo lxc-info -iH $containerName)
 
-#cp over .download file from container 
-
+#cp over .download file from container
+sudo cp /var/lib/lxc/$containerName/rootfs/var/log/.downloads /home/student/$containerName/downloaded 
+ 
 #removes any tail stuff
 TAIL_PID = $!
 
@@ -27,45 +30,56 @@ sudo iptables --table nat --delete POSTROUTING --source $containerIP --destinati
 
 #removes MITM rules
 sudo iptables --table nat --delete PREROUTING --source 0.0.0.0/0 --destination "$externalIP" --protocol tcp --dport 22 --jump DNAT --to-destination "$hostIP:$port"
-sudo iptables --table nat --delete POSTROUTING --source "$hostIP:$port" --destination 0.0.0.0/0 --protocol tcp --dport 22 --jump SNAT --to-source "$externalIP" 
+sudo iptables --table nat --delete POSTROUTING --source "$hostIP:$port" --destination 0.0.0.0/0 --protocol tcp --dport 22 --jump SNAT --to-source "$externalIP"
 
-#stops and deletes container
+#stops container
 sudo lxc-stop -n $containerName
+
+#kills the MITM for this container
+taskNum=`sudo forever list | grep $containerName | cut -d "[" -f 2 | cut -d "]" -f 1`
+sudo forever stop $taskNum
+
+#deletes the container
 sudo lxc-destroy -n $containerName
 
-#creates a file to store mitm logs
-folderName=`date "+%m-%d"`
-fileName= `date "+%H:%M:%S"` + $container_name
 
-if [! -d "$folderName" ]; then
-    mkdir /home/student/logs/$folderName
+#folder name based on the current date
+folderName=`date "+%m-%d"`
+
+#file name based on time of container creation and container name
+fileName= `date "+%H:%M:%S"`
+
+#checks if a folder for the current day already exists
+if [! -d "/home/student/$containerName/$folderName" ]; then
+   mkdir /home/student/$containerName/$folderName
 fi
 
-logName=/home/student/logs/$folderName/$fileName
+logName=/home/student/$containerName/$folderName/$fileName  
 touch $logName
 
 hostIP="127.0.0.1"
-port=12345  
+port=$((RANDOM % 500 + 1300))  
 
+# creates the container
 sudo lxc-create -n $containerName -t download -- -d ubuntu -r focal -a amd64
 sudo lxc-start -n $containerName
 sleep 10
 containerIP=$(sudo lxc-info -iH $containerName)
 
-#starts up MITM server for this honeypot
+# launches the MITM server
 sudo sysctl -w net.ipv4.conf.all.route_localnet=1
-sudo forever -l $logName -a start "$(/home/student/MITM/mitm.js)" -n $containerName -i $containerIP -p $port --auto-access --auto-access-fixed 2 --debug
+sudo forever -l $logName -a start "/home/student/MITM/mitm.js" -n $containerName -i $containerIP -p $port --auto-access --auto-access-fixed 2 --debug
 
-#added nat rules
+# adds iptables rules for the container to the external IP
 sudo ip addr add $externalIP/$prefix brd + dev "eth0"
 sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination $externalIP --jump DNAT --to-destination $containerIP
 sudo iptables --table nat --insert POSTROUTING --source $containerIP --destination 0.0.0.0/0 --jump SNAT --to-source $externalIP
 
-#added MITM rules
+# adds iptables rules for MITM
 sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination "$externalIP" --protocol tcp --dport 22 --jump DNAT --to-destination "$hostIP:$port"
 sudo iptables --table nat --insert POSTROUTING --source "$hostIP:$port" --destination 0.0.0.0/0 --protocol tcp --dport 22 --jump SNAT --to-source "$externalIP" 
 
-#poision wget
+#poison wget
 sudo lxc-attach -n $containerName -- mkdir -p "/var/log/.downloads"
 sudo lxc-attach -n $containerName -- sh -c "cp '/usr/bin/wget' '/usr/bin/wgetOG'"
 sudo lxc-attach -n $containerName -- sh -c "chmod u+x /usr/bin/wgetOG"
@@ -80,8 +94,15 @@ sudo lxc-attach -n $containerName -- sh -c "echo 'NOW=$( date '+%F_%H:%M:%S' )' 
 sudo lxc-attach -n $containerName -- sh -c "echo 'curlOG -O /var/log/.downloads/\$NOW-$containerName \$@ -q /dev/null 2>&1' >> '/usr/bin/curl'"
 sudo lxc-attach -n $containerName -- sh -c "echo 'wgetOG \$@' >> '/usr/bin/curl'"
 
-#cp command to copy honey 
-#add firewall rules held inside a file and check for fire wall rules that need to be added
+#cp command to copy honey
+sudo cp -r /home/student/honey/Department1 /var/lib/lxc/$containerName/rootfs/home/
+sudo cp -r  /home/student/honey/Department2 /var/lib/lxc/$containerName/rootfs/home/
+sudo cp -r  /home/student/honey/Department3 /var/lib/lxc/$containerName/rootfs/home/
+sudo cp -r  /home/student/honey/Department4 /var/lib/lxc/$containerName/rootfs/home/
+sudo cp -r  /home/student/honey/Department5 /var/lib/lxc/$containerName/rootfs/home/
+
+timerLog=/home/student/$containerName/timer.txt
+echo “00:00” > $timerLog
 
 endTime=$(date -Iseconds)
 
